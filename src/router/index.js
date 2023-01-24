603964c4ee5d662800cf9501997eb8864e5738da
unknown <minhaz217@gmail.com>
2023-01-24 21:38:37 +0600

    initiat the router and tailwind css

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,24 @@
 import {createRouter,createWebhistory} from 'vue-router'
+import Home from '../views/Home.vue'
+import NotFound from '../views/NotFound.vue'
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/:catchAll(.*)',
+        name: 'NotFound',
+        component: NotFound
+    }
+]
+
+const router = createRouter({
+    history: createWebhistory(),
+    routes
+})
+
+export default router
---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h2>Page Not Found - 404</h2>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
